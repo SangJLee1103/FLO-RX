d4441eec08f7b28a1e29e863bdf83708c7e7626d
SangJLee1103 <jesusfollow1103@naver.com>
2024-03-16 14:17:00 +0900

    feat: splash  화면 구현 (2초 delay)

--- FLO-RX/SceneDelegate.swift
+++ FLO-RX/SceneDelegate.swift
@@ -11,17 +11,20 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        
         guard let scene = (scene as? UIWindowScene) else { return }
         
-        let window = UIWindow(windowScene: scene)
-        window.rootViewController = ViewController()
-        self.window = window
-        window.makeKeyAndVisible()
+        window = UIWindow(windowScene: scene)
+        window?.rootViewController = UIStoryboard(name: "LaunchScreen", bundle: nil).instantiateInitialViewController()
+        window?.makeKeyAndVisible()
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            self.window?.rootViewController = MusicPlayViewController()
+        }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
